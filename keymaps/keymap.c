f0bfd48e665a6cff5443b5041fb4969f9da2665c
Mateo Bošnjak <bosnjakmateo1@gmail.com>
2023-09-16 21:51:45 +0200

    numpad numbers to normal numbers

--- keymaps/keymap.c
+++ keymaps/keymap.c
@@ -130,9 +130,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  _______, _______,    _______, _______
    ),
    [_NUM] = LAYOUT(
-      _______, _______, _______, _______, _______,    KC_PEQL,   KC_P7,   KC_P8,   KC_P9, KC_PPLS,
-      _______, _______, _______, _______, _______,    KC_PAST,   KC_P4,   KC_P5,   KC_P6, KC_PMNS,
-      _______, _______,  KC_TAB, KC_BSPC,  KC_ENT,      KC_P0,   KC_P1,   KC_P2,   KC_P3, KC_PSLS,
+      _______, _______, _______, _______, _______,    KC_PEQL,    KC_7,    KC_8,    KC_9, KC_PPLS,
+      _______, _______, _______, _______, _______,    KC_PAST,    KC_4,    KC_5,    KC_6, KC_PMNS,
+      _______, _______,  KC_TAB, KC_BSPC,  KC_ENT,       KC_0,    KC_1,    KC_2,    KC_3, KC_PSLS,
                                  _______, _______,    _______, _______
    ),
    [_SPE] = LAYOUT(
