2372476d7ac2cba30f66ae45f8ca6d92346d0178
Mateo Bošnjak <bosnjakmateo1@gmail.com>
2023-10-06 19:46:42 +0200

    Fixed comma and dot on number layout

--- keymaps/keymap.c
+++ keymaps/keymap.c
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_NUM] = LAYOUT(
       _______, _______, _______, KC_PAST, KC_PPLS,    _______,    KC_7,    KC_8,    KC_9, _______,
       _______, _______, _______, KC_PSLS, KC_PMNS,    _______,    KC_4,    KC_5,    KC_6, _______,
-      _______, _______, KC_PEQL, KC_PCMM, KC_PDOT,       KC_0,    KC_1,    KC_2,    KC_3, _______,
+      _______, _______, KC_PEQL, KC_COMM,  KC_DOT,       KC_0,    KC_1,    KC_2,    KC_3, _______,
                                  _______, _______,    _______, _______
    ),
    [_SPE] = LAYOUT(
