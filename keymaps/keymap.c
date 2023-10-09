ee222ca44a6b4f5ff964f7419a25191bd7d173cf
Mateo Bošnjak <bosnjakmateo1@gmail.com>
2023-10-09 21:49:30 +0200

    Well...

--- keymaps/keymap.c
+++ keymaps/keymap.c
@@ -168,12 +168,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    ),
 };
 
-// OLED
 #ifdef OLED_ENABLE
 
 // Rotate OLED
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    return OLED_ROTATION_270;
+    return OLED_ROTATION_90;
 }
 
 // Draw to OLED
@@ -181,21 +180,8 @@ bool oled_task_user() {
     // Set cursor position
     oled_set_cursor(0, 1);
 
-    // Switch on current active layer
-    switch (get_highest_layer(layer_state)) {
-        case 0:
-            oled_write("Base    ", false);
-            break;
-        case 1 :
-            oled_write("Extended", false);
-            break;
-        case 2 :
-            oled_write("Function", false);
-            break;
-        case 3 :
-            oled_write("Symbol  ", false);
-            break;
-    }
+    // Write text to OLED
+    oled_write("Hello World!", false);
 
     return false;
 }
