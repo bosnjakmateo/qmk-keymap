f474ce3ecb1742cfc2ae26ca365a917fa20108e7
Mateo Bošnjak <bosnjakmateo1@gmail.com>
2023-10-09 21:14:24 +0200

    Removed some stuff

--- keymaps/keymap.c
+++ keymaps/keymap.c
@@ -70,51 +70,6 @@ enum custom_keycodes {
     SLACK,
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-   [_BASE] = LAYOUT(
-         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,
-        SFT_A,   CTL_S,   ALT_D,   GUI_F,    KC_G,       KC_H,   GUI_J,   ALT_K,   CTL_L, SFT_SCLN,
-         KC_Y,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_QUES,
-                  LT(FNC, KC_TAB), LT(EXT, KC_SPC),      LT(SYM, KC_ENT),LT(NUM, KC_BSPC)
-   ),
-   [_EXT] = LAYOUT(
-       KC_ESC, KC_PAUS,  KC_INS, GUI_TAB,   HYP_0,    KC_PGUP, KC_HOME,   KC_UP,  KC_END, CW_TOGG,
-      _______, _______, _______, _______, _______,    KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL,
-         UNDO,     CUT,    COPY, KC_LGUI,   PASTE,     KC_ENT, KC_BSPC,  KC_TAB, _______, KC_PSCR,
-                                 _______, _______,    _______, _______
-   ),
-   [_FNC] = LAYOUT(
-      _______, KC_MRWD, KC_MPLY, KC_MFFD, _______,     KC_F12,   KC_F7,   KC_F8,   KC_F9, _______,
-      _______, _______, _______, _______, _______,     KC_F11,   KC_F4,   KC_F5,   KC_F6, _______,
-      _______, KC_VOLD, _______, KC_VOLU, _______,     KC_F10,   KC_F1,   KC_F2,   KC_F3, _______,
-                                 _______, _______,    _______, _______
-   ),
-   [_SYM] = LAYOUT(
-      KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_LBRC,    KC_RBRC, KC_PLUS, KC_ASTR, KC_SLSH, KC_PERC,
-      LSFT_T(KC_AMPR), LCTL_T(KC_UNDS), LALT_T(KC_DQUO), LGUI_T(KC_QUOT), KC_LPRN,    KC_RPRN, KC_MINS, KC_LABK, KC_RABK, KC_SCLN,
-      KC_PIPE, KC_TILD, KC_CIRC,  KC_GRV, KC_LCBR,    KC_RCBR,  KC_EQL, KC_COMM,  KC_DOT, KC_BSLS,
-                                 _______, _______,    _______, _______
-   ),
-   [_NUM] = LAYOUT(
-      _______, _______, _______, KC_PAST, KC_PPLS,    _______,    KC_7,    KC_8,    KC_9, _______,
-      _______, _______, _______, LGUI_T(KC_PSLS), KC_PMNS,    _______,    KC_4,    KC_5,    KC_6, _______,
-      _______, _______, KC_PEQL, KC_COMM,  KC_DOT,       KC_0,    KC_1,    KC_2,    KC_3, _______,
-                                 _______, _______,    _______, _______
-   ),
-   [_SPE] = LAYOUT(
-      _______, _______, _______, _______, _______,      HYP_1,   HYP_2,   HYP_3,   HYP_4,   HYP_5,
-      _______, _______, _______, _______, _______,    _______,    IDEA,  CHROME,   SLACK, _______,
-      _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______,
-                                 _______, _______,    _______, _______
-   ),
-   [_MUS] = LAYOUT(
-      _______, _______, _______, _______, _______,    KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, _______,
-      _______, KC_BTN1, KC_BTN3, KC_BTN2, _______,    KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______,
-      _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______,
-                                 _______, _______,    _______, _______
-   ),
-};
-
 void openApplication(char* applicationName) {
     SEND_STRING(SS_DELAY(100));
     SEND_STRING(SS_DOWN(X_LGUI));
@@ -168,4 +123,49 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         break;
     return true;
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+   [_BASE] = LAYOUT(
+         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,
+        SFT_A,   CTL_S,   ALT_D,   GUI_F,    KC_G,       KC_H,   GUI_J,   ALT_K,   CTL_L, SFT_SCLN,
+         KC_Y,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_QUES,
+                  LT(FNC, KC_TAB), LT(EXT, KC_SPC),      LT(SYM, KC_ENT),LT(NUM, KC_BSPC)
+   ),
+   [_EXT] = LAYOUT(
+       KC_ESC, KC_PAUS,  KC_INS, GUI_TAB,   HYP_0,    KC_PGUP, KC_HOME,   KC_UP,  KC_END, CW_TOGG,
+      _______, _______, _______, _______, _______,    KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  KC_DEL,
+         UNDO,     CUT,    COPY, KC_LGUI,   PASTE,     KC_ENT, KC_BSPC,  KC_TAB, _______, KC_PSCR,
+                                 _______, _______,    _______, _______
+   ),
+   [_FNC] = LAYOUT(
+      _______, KC_MRWD, KC_MPLY, KC_MFFD, _______,     KC_F12,   KC_F7,   KC_F8,   KC_F9, _______,
+      _______, _______, _______, _______, _______,     KC_F11,   KC_F4,   KC_F5,   KC_F6, _______,
+      _______, KC_VOLD, _______, KC_VOLU, _______,     KC_F10,   KC_F1,   KC_F2,   KC_F3, _______,
+                                 _______, _______,    _______, _______
+   ),
+   [_SYM] = LAYOUT(
+      KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_LBRC,    KC_RBRC, KC_PLUS, KC_ASTR, KC_SLSH, KC_PERC,
+      LSFT_T(KC_AMPR), LCTL_T(KC_UNDS), LALT_T(KC_DQUO), LGUI_T(KC_QUOT), KC_LPRN,    KC_RPRN, KC_MINS, KC_LABK, KC_RABK, KC_SCLN,
+      KC_PIPE, KC_TILD, KC_CIRC,  KC_GRV, KC_LCBR,    KC_RCBR,  KC_EQL, KC_COMM,  KC_DOT, KC_BSLS,
+                                 _______, _______,    _______, _______
+   ),
+   [_NUM] = LAYOUT(
+      _______, _______, _______, KC_PAST, KC_PPLS,    _______,    KC_7,    KC_8,    KC_9, _______,
+      _______, _______, _______, LGUI_T(KC_PSLS), KC_PMNS,    _______,    KC_4,    KC_5,    KC_6, _______,
+      _______, _______, KC_PEQL, KC_COMM,  KC_DOT,       KC_0,    KC_1,    KC_2,    KC_3, _______,
+                                 _______, _______,    _______, _______
+   ),
+   [_SPE] = LAYOUT(
+      _______, _______, _______, _______, _______,      HYP_1,   HYP_2,   HYP_3,   HYP_4,   HYP_5,
+      _______, _______, _______, _______, _______,    _______,    IDEA,  CHROME,   SLACK, _______,
+      _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______,
+                                 _______, _______,    _______, _______
+   ),
+   [_MUS] = LAYOUT(
+      _______, _______, _______, _______, _______,    KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, _______,
+      _______, KC_BTN1, KC_BTN3, KC_BTN2, _______,    KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______,
+      _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______,
+                                 _______, _______,    _______, _______
+   ),
 };
