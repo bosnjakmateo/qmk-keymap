babc9e4a916c5e617e03c8417f1645b1b74a1698
Mateo Bošnjak <bosnjakmateo1@gmail.com>
2023-10-23 18:11:45 +0200

    test

--- keymaps/keymap.c
+++ keymaps/keymap.c
@@ -80,17 +80,17 @@ void openApplication(char* applicationName) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case HYPR(ALT_K):
+    case HYPR(KC_U):
         if (record->event.pressed) {
             openApplication("chr");
         }
         break;
-    case HYPR(GUI_J):
+    case HYPR(KC_I):
         if (record->event.pressed) {
             openApplication("idea");
         }
         break;
-    case HYPR(CTL_L):
+    case HYPR(KC_O):
         if (record->event.pressed) {
             openApplication("slack");
         }
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_F12,   KC_F7,   KC_F8,   KC_F9, _______,     _______, KC_7, KC_8, KC_9, _______,
       KC_F11,   KC_F4,   KC_F5,   KC_F6, _______,     _______, KC_4, KC_5, KC_6, KC_COMM,
       KC_F10,   KC_F1,   KC_F2,   KC_F3, _______,      KC_0, KC_1, KC_2, KC_3,    KC_DOT,
-                                 _______, _______,    _______, _______
+                                 _______, KC_LGUI,    _______, _______
    ),
    [_SYM] = LAYOUT(
       KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_LBRC,    KC_RBRC, KC_PLUS, KC_ASTR, KC_SLSH, KC_PERC,
